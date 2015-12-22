2d8cb2677e9f3831248c67111f251d349351e517
John Drago <jdrago.999@gmail.com>
2015-12-22 00:32:08 -0800

    Some basic pages added. More legwork is required to expose active handlers and workers.

--- config/routes.rb
+++ config/routes.rb
@@ -4,28 +4,28 @@ Rails.application.routes.draw do
   scope 'workflows' do
     get '' => 'workflow#list'
     scope 'new' do
-      get '' => 'workflow#create'
-      post '' => 'workflow#submit_create'
+      get '' => 'workflow#create', as: :create_workflow
+      post '' => 'workflow#create'
     end
     scope ':workflow_id' do
-      get '' => 'workflow#show'
-      post 'cancel' => 'workflow#cancel'
-      post 'pause' => 'workflow#pause'
-      post 'resume' => 'workflow#resume'
+      get '' => 'workflow#show', as: :show_workflow
+      post 'cancel' => 'workflow#cancel', as: :cancel_workflow
+      post 'pause' => 'workflow#pause', as: :pause_workflow
+      post 'resume' => 'workflow#resume', as: :resume_workflow
     end
   end
 
   scope 'handlers' do
     get '' => 'handler#list'
     scope ':name' do
-      get '' => 'handler#show'
+      get '' => 'handler#show', as: :show_handler
     end
   end
 
   scope 'workers' do
     get '' => 'worker#list'
     scope ':worker_id' do
-      get '' => 'worker#show'
+      get '' => 'worker#show', as: :show_worker
     end
   end
 end
--- spec/controllers/workflow_controller_spec.rb
+++ spec/controllers/workflow_controller_spec.rb
@@ -2,11 +2,28 @@ require 'spec_helper'
 
 describe WorkflowController do
   describe '#list' do
-    before do
-      get :list
-    end
-    it 'renders the correct template' do
-      expect(response).to render_template :list
+    context 'when there' do
+      context 'are workflows' do
+        before do
+          expect(Distribot::Workflow).to receive(:active) do
+            workflow = double('workflow')
+            [ workflow ]
+          end
+          get :list
+        end
+        it 'lists them' do
+          expect(response).to render_template :list
+        end
+      end
+      context 'are no workflows' do
+        before do
+          expect(Distribot::Workflow).to receive(:active){ [] }
+          get :list
+        end
+        it 'shows the empty state' do
+          expect(response).to render_template :empty_list
+        end
+      end
     end
   end
 end
